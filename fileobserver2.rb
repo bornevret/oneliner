98f738206ffc9322fad71cd30b84d967049b6b77
bornevret <62755235+bornevret@users.noreply.github.com>
2020-04-24 14:53:51 +0200

    Add files via upload

---
+++ fileobserver2.rb
@@ -0,0 +1,30 @@
+class FileObserver2
+    
+    def initialize(target_dir)
+        @file_inventory = {}
+        @changed_files = []
+        @new_files = []
+        @deleted_files = []
+        @target_dir = target_dir
+    end
+    
+    def update_inventory
+        path = File.join( @target_dir, "**/*" )
+        files = Dir.glob(path)
+        files.each do |file|
+            if File.file?( file )
+                if @file_inventory.has_key?( file )
+                    if mtime != @file_inventory( file )
+                        @changed_files << file
+                        @file_inventory = File.mtime(file)
+                    end
+                else 
+                    @file_inventory = File.mtime( file )
+                    @new_files << file 
+                end
+            end
+        end
+        
+    def get_deleted_files
+        @file_inventory.each do |file,mtime|
+            if @file_inventory 
